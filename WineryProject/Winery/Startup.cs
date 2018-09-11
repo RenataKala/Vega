d0e2427a082ea51816926f13b750af7b4632b3e4
Renata Kalamatkovska <renata.tanusheva@gmail.com>
2018-09-11 20:42:42 +0200

    Initial empty MVC app

---
+++ WineryProject/Winery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Winery.Startup))]
+namespace Winery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
