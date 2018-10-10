ab8eff9e69d2653af17aa2415ea583fecea92e40
Marija Dudan <marijadudanovska@gmail.com>
2018-10-10 14:24:47 +0200

    wine-facts

--- WineryProject/Winery/Controllers/HomeController.cs
+++ WineryProject/Winery/Controllers/HomeController.cs
@@ -26,9 +26,9 @@ namespace Winery.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult WineFacts()
         {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
         }
